19728e0b4a0b14ed6084b44565d03127fd16dbde
Madimo Zhang <MadimoZhang@gmail.com>
2013-12-24 18:13:35 +0800

    Finish API: login

---
+++ NewAnimeChecker/AnimeAPI.h
@@ -0,0 +1,19 @@
+//
+//  AnimeAPI.h
+//  NewAnimeChecker
+//
+//  Created by Madimo on 13-12-24.
+//  Copyright (c) 2013年 Madimo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AnimeAPI : NSObject {
+    NSString *key;
+}
+
+- (void)loginWithUsername:(NSString *)username password:(NSString *)password;
+
+- (void)processErrorWithResult:(NSString *)result;
+
+@end
