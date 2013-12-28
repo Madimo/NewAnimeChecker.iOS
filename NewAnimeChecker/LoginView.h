97dc0c2aaa04be07cfe30abb4be33d688b142060
Madimo Zhang <MadimoZhang@gmail.com>
2013-12-28 20:38:03 +0800

    Login view

---
+++ NewAnimeChecker/LoginView.h
@@ -0,0 +1,13 @@
+//
+//  LoginView.h
+//  NewAnimeChecker
+//
+//  Created by Madimo on 13-12-27.
+//  Copyright (c) 2013年 Madimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginView : UIView
+
+@end
---
+++ NewAnimeChecker/LoginView.m
@@ -0,0 +1,31 @@
+//
+//  LoginView.m
+//  NewAnimeChecker
+//
+//  Created by Madimo on 13-12-27.
+//  Copyright (c) 2013年 Madimo. All rights reserved.
+//
+
+#import "LoginView.h"
+
+@implementation LoginView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
